44c378d7a8ce9277b2e11a6583fdc118238fb247
Srivatsa Rv <43114746+SrivatsaRv@users.noreply.github.com>
2021-03-12 12:56:01 +0530

    Update parser_t3.sh

--- Week 2/Shell Scripts (8 Tasks)/parser_t3.sh
+++ Week 2/Shell Scripts (8 Tasks)/parser_t3.sh
@@ -1,69 +1,42 @@
-OGFILE="/Users/srivatsa.intern/Desktop/access.log" # NGINX LOGFILE
+NGINXLOG="/Users/srivatsa.intern/Desktop/access.log" # NGINX LOGFILE
 echo""
-echo "3.Top requests:"
-echo "==============="
-echo""
-echo "*top 5 requests by upstream_ip:"
-cat $LOGFILE \
-| awk '{print $9}'\
-| sort \
-| uniq -c \
-| sort -r \
-| head -5
+echo "3.Top Requests:"
+echo "--------------------------------------------------"
+echo " 3.1 - Top 5 Reuqests From Upstream IP Address:"
+echo "--------------------------------------------------"
+cat $NGINXLOG | awk '{print $9}'| sort | sort -r | head -5| uniq -c 
+echo ""
 
-
-echo""
-
-echo "*top 5 requests by host:"
-cat $LOGFILE \
-| awk '{print $NF}'\
-| sort \
-| uniq -c \
-| sort -r \
-| head -5
-
-echo""
-
-
-echo "*top 5 requests by bodyBytesSent:"
-cat $LOGFILE \
-| awk '{print $10}'\
-| sort \
-| uniq -c \
-| sort -r \
-| head -5
-
-echo""
-
-echo "*top 5 requests by path:"
-cat $LOGFILE \
-| awk '{print $5}'\
-| sort \
-| uniq -c \
-| sort -r \
-| head -5
+echo "----------------------------------"
+echo " 3.2 - Top 5 Reuqests From Hosts:"
+echo "----------------------------------"
+cat $NGINXLOG| awk '{print $NF}'| sort | sort -r | head -5 | uniq -c 
 
 
 echo""
 
-echo "*top 5 requests with the highest response time"
-cat $LOGFILE \
-| awk '{print $8}'\
-| sort \
-| uniq -c \
-| sort -r \
-| head -5
-
+echo "----------------------------------------"
+echo " 3.3 - Top 5 Requests By BodyBytesSent:"
+echo "----------------------------------------"
+cat $NGINXLOG| awk '{print $10}'| sort | sort -r | head -5 | uniq -c 
 echo""
 
-echo "*get top 5 requests returning 200/5xx/4xx per host"
-cat $LOGFILE \
-| awk '{print $7}'\
-| sort \
-| uniq -c \
-| sort \
-| head -5
+echo "----------------------------------------"
+echo " 3.4 - Top 5 Requests From Path:"
+echo "----------------------------------------"
+cat $NGINXLOG | awk '{print $5}'| sort | sort -r | head -5 | uniq -c 
 
+echo""
+echo "-----------------------------------------------------"
+echo " 3.5 - Top 5 Requests Having Highest Response Time:"
+echo "-----------------------------------------------------"
+cat $NGINXLOG | awk '{print $8}'| sort | sort -r | head -5 | uniq -c 
+ 
+echo""
+echo "------------------------------------------------------"
+echo " 3.6 - Top 5 Requests Returning 200/5xx/4xx Per Host:"
+echo "------------------------------------------------------"
+cat $NGINXLOG | awk '{print $7}'|sort | sort -r | head -5 | uniq -c 
 echo""
 
 
