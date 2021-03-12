d6159f47e9796a30b1bcf32d5b81eeee5252215f
Srivatsa Rv <43114746+SrivatsaRv@users.noreply.github.com>
2021-03-12 12:52:14 +0530

    Update parser_t2.sh

--- Week 2/Shell Scripts (8 Tasks)/parser_t2.sh
+++ Week 2/Shell Scripts (8 Tasks)/parser_t2.sh
@@ -1,10 +1,8 @@
-LOGFILE="/Users/srivatsa.intern/Desktop/access.log" # NGINX LOGFILE
-echo "2.Total requests per status code"
-echo "================================="
-echo"" 
-cat  $LOGFILE \
-| awk '{print $7}'\
-| sort \
-| uniq -c \
-| sort 
+NGINXLOG="/Users/srivatsa.intern/Desktop/access.log" # NGINX LOGFILE
+echo ""
+echo "2.Total Requests Per Staus Code: (# of Requests Returning 404/401/502/504/500/200):"
+echo "===================================================================================="
+echo "  #  HTTP"
+cat  $NGINXLOG | awk '{print $7}'| sort | uniq -c | sort 
+#$7 is the column for status code reuqests.
 
