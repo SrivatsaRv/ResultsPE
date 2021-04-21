89a34954d55fa5896d2d72b9e3d886b7ce1b6fd1
Srivatsa Rv <43114746+SrivatsaRv@users.noreply.github.com>
2021-04-21 13:47:58 +0530

    Delete Installation Scripts directory

--- Installation Scripts/aerospike_installer.sh
+++
@@ -1,8 +0,0 @@
-wget -O aerospike.tgz 'http://cor-mirror101.phonepe.nm5/aerospike-E4.8.0.6.tgz'
-apt-get update
-tar -xvf aerospike.tgz
-cd aerospike-server-enterprise-4.8.0.6-ubuntu16.04/
-sudo ./asinstall
-
-wget -O amc.deb 'http://cor-mirror101.phonepe.nm5/aerospike-amc-e4.0.24.deb'
-dpkg -i amc.deb
