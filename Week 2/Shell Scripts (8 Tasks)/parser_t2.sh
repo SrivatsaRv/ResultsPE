00bfee8895c21f93a17bdc69aed3b2cc011543b7
Srivatsa Rv <43114746+SrivatsaRv@users.noreply.github.com>
2021-04-21 13:47:32 +0530

    Delete Week 2/Shell Scripts (8 Tasks) directory

--- Week 2/Shell Scripts (8 Tasks)/parser_t2.sh
+++
@@ -1,8 +0,0 @@
-NGINXLOG="/Users/srivatsa.intern/Desktop/access.log" # NGINX LOGFILE
-echo ""
-echo "2.Total Requests Per Staus Code: (# of Requests Returning 404/401/502/504/500/200):"
-echo "===================================================================================="
-echo "  #  HTTP"
-cat  $NGINXLOG | awk '{print $7}'| sort | uniq -c | sort 
-#$7 is the column for status code reuqests.
-
--- Week 2/Shell Scripts (8 Tasks)/parser_t4.sh
+++
@@ -1,6 +0,0 @@
-echo "TASK - 4"
-echo "-----------------------------------------"
-echo "PRINT STATUS CODES RECEIVED BY EACH HOST"
-echo "-----------------------------------------"
-echo "HOST:CODE"
-awk '{ print $15 " " $7 }' access.log |sort | uniq -c | sort -rn
